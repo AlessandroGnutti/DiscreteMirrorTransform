44e23a85c002a95d363f5e39ddd863294eb0c8b6
AlessandroGnutti <alessandro.gnutti@unibs.it>
2022-04-15 15:16:49 +0200

    Main seism

---
+++ testSeismic.m
@@ -0,0 +1,108 @@
+clear
+close all
+clc
+
+klt_basis = load("IN/KLT_seismic.mat");
+sot_basis = load("IN/SOT_seismic.mat");
+
+dwtmode('per')
+wavelet_name = 'db4';
+
+fld_seismic = 'Set sisma testing/';
+seismicbase = dir(fullfile(fld_seismic, '*.sac'));
+
+% perc = 0.005:0.005:0.1;
+perc = 0.01:0.01:0.15;
+% perc = 0.05;
+
+mse_dmt = zeros(length(seismicbase),length(perc));
+mse_dct = zeros(length(seismicbase),length(perc));
+mse_dwt = zeros(length(seismicbase),length(perc));
+mse_klt = zeros(length(seismicbase),length(perc));
+mse_sot = zeros(length(seismicbase),length(perc));
+for ii = 1:length(seismicbase)
+    
+    str = seismicbase(ii).name;
+    disp(str)
+    x = rdsac(strcat(fld_seismic,str));
+    x = x.d;
+
+    M = floor(length(x)/400);
+    x = x(1:M*400);
+
+    x = x-min(x);
+    x = x/max(x);
+ 
+    %%%%% DMT %%%%%
+    fprintf('Mirror Transform loading...\n')
+    [X_dmt, n0_table, flag_table] = mirrorTransform2D_fast(x,'dmt');
+    fprintf('Mirror Transform computed.\n')
+    
+    for k = 1:length(perc)
+        K = round(perc(k)*numel(x));
+        X_nonlinapp = nonLinApp(X_dmt, K);
+%         x_dmt_rec = inverseMirrorTransform2D(X_nonlinapp, n0_table, flag_table);
+        mse_dmt(ii,k) = sum((X_dmt-X_nonlinapp).^2)/numel(x);
+    end
+    
+    %%%%% DCT %%%%%
+    X_dct = dct(x);
+    for k = 1:length(perc)
+        K = round(perc(k)*numel(x));
+        X_dct_nonlinapp = nonLinApp(X_dct, K);
+        x_dct_rec = idct(X_dct_nonlinapp);
+        mse_dct(ii,k) = sum((x-x_dct_rec).^2)/numel(x);
+    end
+    
+    %%%%% DWT %%%%%
+    L = wmaxlev(size(x),wavelet_name);
+    [C,S] = wavedec(x,L,wavelet_name);
+    for k = 1:length(perc)
+        K = round(perc(k)*numel(x));
+        C_nonlinapp = nonLinApp(C, K);
+        x_dwt_rec =  waverec(C_nonlinapp, S, wavelet_name);
+        mse_dwt(ii,k) = sum((x-x_dwt_rec).^2)/numel(x);
+    end
+
+    %%%%% KLT and SOT %%%%%
+    for k = 1:length(perc)
+        K = round(perc(k)*400);
+        mse_total_klt = 0;
+        mse_total_sot = 0;
+        for ww = 1:M
+            x_w = x((ww-1)*400+1:ww*400);
+
+            X_w = klt_basis.K'*x_w;
+            X_nonlinapp = nonLinApp(X_w, K);
+            mse_total_klt = mse_total_klt + sum((X_w-X_nonlinapp).^2);
+
+            X_w = sot_basis.E'*x_w;
+            X_nonlinapp = nonLinApp(X_w, K);
+            mse_total_sot = mse_total_sot + sum((X_w-X_nonlinapp).^2);
+
+
+        end
+        mse_klt(ii,k) = mse_total_klt/numel(x);
+        mse_sot(ii,k) = mse_total_sot/numel(x);
+    end
+    
+end
+
+%%%%% Plot %%%%%
+plot(perc*100, mean(mse_dwt),'r')
+hold on
+plot(perc*100, mean(mse_dct),'k')
+plot(perc*100, mean(mse_dmt),'b')
+plot(perc*100, mean(mse_klt),'m')
+plot(perc*100, mean(mse_sot),'c')
+grid on
+legend('DWT', 'DCT','DMT','KLT','SOT')
+xlabel('% non-zero coefficients'), ylabel('mse')
+% xlim(100*[0.005 0.05])
+
+%%%%%%%%%%%%%%%%%%%%%%%%% FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%
+function y = nonLinApp(x, k)
+[~, ind] = sort(abs(x), 'descend');
+y = zeros(size(x));
+y(ind(1:k)) = x(ind(1:k));
+end
---
+++ trainingSeismic.m
@@ -0,0 +1,45 @@
+clear
+close all
+clc
+
+fld_seismic = 'Set sisma training/';
+seismicbase = dir(fullfile(fld_seismic, '*.sac'));
+
+% 400-sample windows
+L = 400;
+
+% Count the number of windows
+total = 0;
+for ii = 1:length(seismicbase)
+
+    str = seismicbase(ii).name;
+    disp(str)
+    x = rdsac(strcat(fld_seismic,str));
+    x = x.d;
+    total = total + floor(length(x)/L);
+
+end
+
+X = zeros(total,L);
+count = 1;
+for ii = 1:length(seismicbase)
+
+    str = seismicbase(ii).name;
+    disp(str)
+    x = rdsac(strcat(fld_seismic,str));
+    x = x.d;
+    x = x-min(x);
+    x = x/max(x);
+
+    for jj = 1:floor(length(x)/L)
+
+        X(count,:) = x((jj-1)*L+1:jj*L);
+        count = count + 1;
+    end
+
+end
+
+C = cov(X);
+[K,D] = eig(C);
+save('In/Training seismic sequences.mat','X')
+save('In/KLT_seismic.mat','K')
